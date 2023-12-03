1bb016644fdd185d9845f5007daeb8dc785eb676
Ronaldo Bicca <ronaldo.bicca@gmail.com>
2023-12-03 17:18:24 -0300

    Tabs addedd but with props chain

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:pocnav/data/dummy_data.dart';
 import 'package:pocnav/models/category.dart';
+import 'package:pocnav/models/meal.dart';
 import 'package:pocnav/screens/meals.dart';
 import 'package:pocnav/widgets/category_grid_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
-  const CategoriesScreen({super.key});
+  const CategoriesScreen({super.key, required this.onToggleFavorite});
+  final void Function(Meal meal) onToggleFavorite;
 
   void _selectCategory(BuildContext ctx, Category category) {
     final filteredMeals =
@@ -14,32 +16,31 @@ class CategoriesScreen extends StatelessWidget {
     Navigator.push(
         ctx,
         MaterialPageRoute(
-            builder: (ctx) =>
-                MealsScreen(title: category.title, meals: filteredMeals)));
+            builder: (ctx) => MealsScreen(
+                  title: category.title,
+                  meals: filteredMeals,
+                  onToggleFavorite: onToggleFavorite,
+                )));
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: const Text('Escolha sua categoria'),
-        ),
-        body: GridView(
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2,
-              childAspectRatio: 3 / 2,
-              crossAxisSpacing: 20,
-              mainAxisSpacing: 20),
-          padding: const EdgeInsets.all(24),
-          children: [
-            for (final c in availableCategories)
-              CategoryGridItem(
-                category: c,
-                onSelectCategory: () {
-                  _selectCategory(context, c);
-                },
-              )
-          ],
-        ));
+    return GridView(
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20),
+      padding: const EdgeInsets.all(24),
+      children: [
+        for (final c in availableCategories)
+          CategoryGridItem(
+            category: c,
+            onSelectCategory: () {
+              _selectCategory(context, c);
+            },
+          )
+      ],
+    );
   }
 }
---
+++ lib/screens/tabs.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:pocnav/models/meal.dart';
+import 'package:pocnav/screens/categories.dart';
+import 'package:pocnav/screens/meals.dart';
+
+class TabsScreen extends StatefulWidget {
+  const TabsScreen({super.key});
+
+  @override
+  State<TabsScreen> createState() => _TabsScreenState();
+}
+
+class _TabsScreenState extends State<TabsScreen> {
+  int _selectedPageIndex = 0;
+  final List<Meal> _favoriteMeals = [];
+
+  void _selectPage(int index) {
+    setState(() {
+      _selectedPageIndex = index;
+    });
+  }
+
+  void _toggleMealFavoriteStatus(Meal meal) {
+    if (_favoriteMeals.contains(meal)) {
+      _favoriteMeals.remove(meal);
+    } else {
+      _favoriteMeals.add(meal);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Widget activePage = CategoriesScreen(
+      onToggleFavorite: _toggleMealFavoriteStatus,
+    );
+    var activePageTitle = 'Categorias';
+
+    if (_selectedPageIndex == 1) {
+      activePage = MealsScreen(
+        meals: [],
+        onToggleFavorite: _toggleMealFavoriteStatus,
+      );
+      activePageTitle = 'Seus favoritos';
+    }
+
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(activePageTitle),
+        ),
+        body: activePage,
+        bottomNavigationBar: BottomNavigationBar(
+          onTap: _selectPage,
+          currentIndex: _selectedPageIndex,
+          items: const [
+            BottomNavigationBarItem(
+                icon: Icon(Icons.set_meal), label: 'Cateogrias'),
+            BottomNavigationBarItem(icon: Icon(Icons.star), label: 'Favoritos'),
+          ],
+        ));
+  }
+}
